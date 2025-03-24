efde95319b3debd2a8be7727108ae9e0c192c052
ViruS Spider <sras0202@mail.ru>
2025-03-24 23:24:18 +0500

    feat(index): add new routes

--- criptoExcgangeBackend/index.js
+++ criptoExcgangeBackend/index.js
@@ -3,17 +3,19 @@ const cors = require('cors');
 
 const app = express();
 
-// объявление роутеров
-const ExchReqRouter = require('./routes/exchangeRequest.router')
+const ExchReqRouter = require('./routes/exchangeRequest.router');
 const UserRouter = require('./routes/user.router');
+const curencyRouter = require('./routes/currancy_pair.router');
+const feesRouter = require('./routes/fees_limits.router');
 
 const Router = express.Router();
 
 app.use(express.json());
 app.use(cors());
 
-// Имя роутеров
-app.use('/exchangeReq', ExchReqRouter)
-app.use('/users', UserRouter)
+app.use('/exchangeReq', ExchReqRouter);
+app.use('/users', UserRouter);
+app.use('/curencyRouter', curencyRouter);
+app.use('/fees_limit', feesRouter);
 
 app.listen(3000, () => console.log('Сервер работает на порту 3000'));
