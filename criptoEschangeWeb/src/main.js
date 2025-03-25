3481002e33b05d30e82e50cd091c6670507e04d0
ViruS Spider <sras0202@mail.ru>
2025-03-25 23:32:43 +0500

    feat(*): add new packet

--- criptoEschangeWeb/src/main.js
+++ criptoEschangeWeb/src/main.js
@@ -1,13 +1,16 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createVuetify } from 'vuetify'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createVuetify } from 'vuetify';
 import 'vuetify/styles' // Подключение стилей Vuetify
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import router from './router'
-import apiPlugin from './plugins/api.js'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import router from './router';
+import apiPlugin from './plugins/api.js';
+
+import Toast from "vue-toastification";
+import 'vue-toastification/dist/index.css';
 
 const vuetify = createVuetify({
     components,
@@ -18,6 +21,20 @@ const app = createApp(App);
 app.use(router);
 app.use(vuetify)
 app.use(apiPlugin);
+app.use(Toast, {
+  timeout: 3000, // Время показа (мс)
+  position: 'top-right', // Позиция (top-right, bottom-left и т. д.)
+  closeOnClick: true, // Закрывать по клику
+  pauseOnFocusLoss: false, // Не останавливать таймер при потере фокуса
+  pauseOnHover: true, // Пауза при наведении
+  draggable: true, // Можно двигать мышкой
+  draggablePercent: 0.6, // Процент для свайпа
+  showCloseButtonOnHover: false, // Показывать кнопку закрытия при наведении
+  hideProgressBar: false, // Скрыть полосу прогресса
+  closeButton: 'button', // Тип кнопки закрытия ('button' или 'component')
+  icon: true, // Показывать иконку
+  rtl: false, // Справа налево (для арабского и т. д.)
+});
 
 app.provide('$api', app.config.globalProperties.$api);
 
