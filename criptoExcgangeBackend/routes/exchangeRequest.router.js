02e5541b758acb52fef42a159b7374dbcfec964a
SpaiderViruS <emel07827@gmail.com>
2025-04-19 21:35:37 +0500

    fix(exchangeReqRouter): fix routes

--- criptoExcgangeBackend/routes/exchangeRequest.router.js
+++ criptoExcgangeBackend/routes/exchangeRequest.router.js
@@ -4,6 +4,7 @@ const router = new Router();
 const exchangeRequestController = require('../controllers/exchangeRequest.controller');
 
 router.get('/', exchangeRequestController.getAllExcnages);
+router.get('/:uuid', exchangeRequestController.getExchangeByUUID);
 router.post('/', exchangeRequestController.newRequest);
 
 module.exports = router;
